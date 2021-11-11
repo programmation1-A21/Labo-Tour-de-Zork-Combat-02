cec1bdea3cedc6614e3061d185908ea78cd497a0
ftaleb <frederik.taleb@gmail.com>
2021-11-11 11:41:35 -0500

    Programme de base

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Labo_Tour_de_Zork_Combat_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
